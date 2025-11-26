847b03cec1a351431995c9b6451f2be048f42313
Dany Thomas <prog3@infobahnworld.com>
2025-11-26 16:06:58 +0400

    üpdates

--- src/pages/KanbanBoard/CardForm.jsx
+++ src/pages/KanbanBoard/CardForm.jsx
@@ -26,7 +26,7 @@ const TOP_TABS = [
   "KPI",
 ];
 
-const ENABLED_TABS = ["General", "Operation"];
+const ENABLED_TABS = ["General", "Operation", "Husbandry"];
 
 const DEFAULT_ACCENT_COLOR = "#2A00FF";
 const TOTAL_STEPS = 5;
