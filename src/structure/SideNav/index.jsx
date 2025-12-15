731cae50c45cbecd873eb780277cc8f7d5ac058c
Dany Thomas <prog3@infobahnworld.com>
2025-12-15 11:46:36 +0400

    updates

--- src/structure/SideNav/index.jsx
+++ src/structure/SideNav/index.jsx
@@ -34,7 +34,7 @@ function SideNav({ isMobileMenuOpen, onCloseMobileMenu }) {
   const kanbanIcons = [
     { id: 1, icon: GroupIcon, label: 'Add' },
     { id: 2, icon: AnalyticsIcon, label: 'Analytics' },
-    { id: 3, icon: InboxIcon, label: 'Inbox' },
+    { id: 3, icon: InboxIcon, label: 'Workspaces' },
     { id: 4, icon: CalendarIcon, label: 'Calendar' },
     { id: 5, icon: ReportsIcon, label: 'Reports' },
     { id: 6, icon: SettingsIcon, label: 'Settings' },
