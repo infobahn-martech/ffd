7011fbe22fac3836fe3d3c02d61fa4440c0b3b3e
Dany Thomas <prog3@infobahnworld.com>
2026-01-08 09:08:45 +0400

    updates

---
+++ src/pages/TransportParties/Modals/AddEditTransportParties.jsx
@@ -0,0 +1,89 @@
+import { useForm } from "react-hook-form";
+import CustomModal from "../../../components/CustomModal";
+import "../../../design/scss/prospect-modal.scss";
+import "../../../design/scss/modal-designs.scss";
+import "../../../design/scss/form-designs.scss";
+
+export function TransportPartyModal({ showModal, closeModal }) {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        defaultValues: showModal?._id
+            ? {
+                name: showModal?.name,
+            }
+            : {
+                name: "",
+            },
+    });
+
+    const onSubmit = (data) => {
+        console.log("TRANSPORT PARTY FORM SUBMITTED:", data);
+        closeModal();
+    };
+
+    const renderHeader = () => (
+        <>
+            <h1 className="modal-title">
+                {showModal?._id ? "Edit Transport Party" : "Add Transport Party"}
+            </h1>
+            <button
+                type="button"
+                className="btn-close"
+                aria-label="Close"
+                onClick={closeModal}
+            ></button>
+        </>
+    );
+
+    const renderBody = () => (
+        <div className="modal-body">
+            <div className="lead-form">
+                <form id="transportPartyForm" onSubmit={handleSubmit(onSubmit)}>
+                    <div className="permInputs row mb-lg-3">
+                        <div className="col-12 mb-3">
+                            <div className="form-floating desig-inp">
+                                <input
+                                    className={`form-control ${errors.name ? "is-invalid" : ""}`}
+                                    placeholder="Name"
+                                    {...register("name", { required: "Name is required" })}
+                                />
+                                <label>
+                                    Name <span className="text-danger">*</span>
+                                </label>
+                                {errors.name && (
+                                    <span className="error text-danger">{errors.name.message}</span>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+
+    const renderFooter = () => (
+        <div className="modal-footer">
+            <button type="button" className="btn btn-outline" onClick={closeModal}>
+                Close
+            </button>
+            <button type="submit" form="transportPartyForm" className="btn btn-primary">
+                Save
+            </button>
+        </div>
+    );
+
+    return (
+        <CustomModal
+            className="fade transport-party-modal-sm modal show"
+            dialgName="modal-dialog modal-dialog-centered"
+            show={!!showModal}
+            closeModal={() => closeModal(null)}
+            body={renderBody()}
+            footer={renderFooter()}
+            header={renderHeader()}
+        />
+    );
+}
