1b61136ba8d2d0d034b1665b098c12a0312d9583
Dany Thomas <prog3@infobahnworld.com>
2026-03-07 10:33:47 +0400

    updates

--- src/pages/Compact/index.jsx
+++ src/pages/Compact/index.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { FiMoreVertical } from 'react-icons/fi';
+import './Compact.scss';
+
+const ITEMS = [
+  'Marine Port calls',
+  'Crew Change / Material Supply',
+  'L & T Marine and Crew workflow',
+];
 
 export default function Compact() {
   return (
     <div className="compact-page">
-      <h1>Compact</h1>
+      <div className="compact-list">
+        {ITEMS.map((label, index) => (
+          <div key={index} className="compact-list-item">
+            <span className="compact-list-item-text">{label}</span>
+            <button
+              type="button"
+              className="compact-list-item-kebab"
+              aria-label="More options"
+            >
+              <FiMoreVertical size={20} />
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
