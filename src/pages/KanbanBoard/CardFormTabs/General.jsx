fc07dbabd1dd0149fea91a418fa077da152ee93b
Dany Thomas <prog3@infobahnworld.com>
2026-03-14 08:44:49 +0400

    updates

--- src/pages/KanbanBoard/CardFormTabs/General.jsx
+++ src/pages/KanbanBoard/CardFormTabs/General.jsx
@@ -859,7 +859,7 @@ const DailyTaskTodo = ({ tasks = [], onChange, accentColor }) => {
                       className={`daily-task-checkbox-icon ${task.completed ? "checked" : ""}`}
                     >
                       {task.completed && (
-                        <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="10" height="10" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                           <path
                             d="M10 3L4.5 8.5L2 6"
                             stroke="white"
