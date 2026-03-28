d3bad38c3a85b007b036f29fd8abcc0a18974b8f
Dany Thomas <prog3@infobahnworld.com>
2026-03-28 11:35:11 +0400

    updates

--- src/services/transportCompanyService.js
+++ src/services/transportCompanyService.js
@@ -9,7 +9,7 @@ const getTransportCompanyById = (transport_company_id) =>
   Gateway.get(`/transport/get_company_by_id/${transport_company_id}`);
 
 const updateTransportCompanyData = (data) =>
-  Gateway.post(`/transport/update_transport_company/${data.transport_company_id}`, data);
+  Gateway.post(`/transport/update_transport_company`, data);
 
 const deleteTransportCompanyData = (transport_company_id) =>
   Gateway.delete(`/transport/delete_transport_company/${transport_company_id}`);
