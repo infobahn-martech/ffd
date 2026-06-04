3c2825d37886a018fca735b5f478d4cbc05f95e4
infobahn-martech <martech@infobahn.ae>
2026-06-04 12:10:55 +0530

    Fix:Update landing API

--- src/services/landingNoteService.js
+++ src/services/landingNoteService.js
@@ -1,6 +1,6 @@
 import Gateway from '../gateway/gateway';
 
-const getAllLandingNotes = (params) => Gateway.get('/material_management/get_all_landing_notes', { params });
+const getAllLandingNotes = (callId, params) => Gateway.get(`/material_management/get_all_landing_notes/${callId}`, { params });
 const getLandingNoteById = (id) => Gateway.get(`/material_management/get_landing_note_by_id/${id}`);
 const saveLandingNote = (data) => Gateway.post('/material_management/save_landing_note', data);
 const updateLandingNote = (id, data) => Gateway.post(`/material_management/update_landing_note/${id}`, data);
