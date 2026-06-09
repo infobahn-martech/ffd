84d30e674528df333a11faaf97477d7ae2d2f33f
infobahn-martech <martech@infobahn.ae>
2026-06-09 16:43:42 +0530

    fixed API issue

--- src/services/thirdPartyService.js
+++ src/services/thirdPartyService.js
@@ -3,6 +3,6 @@ import Gateway from '../gateway/gateway';
 const addThirdPartyService = (data) => Gateway.post('/service/add_third_party_service', data);
 const getThirdPartyServices = ({ params }) => Gateway.get('/service/get_all_third_party_service', { params });
 const updateThirdPartyService = (data) => Gateway.post(`/service/update_third_party_service`, data);
-const deleteThirdPartyService = (third_party_service_id) => Gateway.post(`/service/delete_third_party_service/${third_party_service_id}`);
+const deleteThirdPartyService = (third_party_service_id) => Gateway.delete(`/service/delete_third_party_service/${third_party_service_id}`);
 
 export default { addThirdPartyService, getThirdPartyServices, updateThirdPartyService, deleteThirdPartyService };
