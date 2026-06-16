0a11012ebc52e71e9ef8dbbe6ec039bcc33b6c93
infobahn-martech <martech@infobahn.ae>
2026-06-16 16:40:26 +0530

    fix api

--- src/services/operatorService.js
+++ src/services/operatorService.js
@@ -15,7 +15,7 @@ const updateOperator = (data) =>
   Gateway.post("/launch_hire/update_operator", data);
 
 const deleteOperator = (operator_id) =>
-  Gateway.post("/launch_hire/delete_taxi_boat_operator", { operator_id });
+  Gateway.delete(`/launch_hire/delete_operator/${operator_id}`);
 
 export default {
   addOperator,
