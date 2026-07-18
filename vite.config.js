b0e9b7b6ea7cf42c4b6389893a2016aa9503bfa3
Dany Thomas <prog3@infobahnworld.com>
2026-07-18 13:21:41 +0400

    Configure cPanel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/sedres-fe/',
+  base: "/",
   build: {
-    outDir: 'dist',
+    outDir: "dist",
   },
 });
