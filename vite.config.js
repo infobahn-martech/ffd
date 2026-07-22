2b1cd6455bb2398d4794c26779f55940136d3358
Dany Thomas <prog3@infobahnworld.com>
2026-07-22 14:21:11 +0400

    updates

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,27 @@ export default defineConfig({
   base: "/",
   build: {
     outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          mui: ["@mui/material", "@mui/x-date-pickers", "@emotion/react", "@emotion/styled"],
+          tiptap: [
+            "@tiptap/react",
+            "@tiptap/starter-kit",
+            "@tiptap/extension-color",
+            "@tiptap/extension-image",
+            "@tiptap/extension-link",
+            "@tiptap/extension-text-align",
+            "@tiptap/extension-text-style",
+            "@tiptap/extension-underline",
+          ],
+          quill: ["quill", "quill-table-better", "react-quill", "react-quill-new"],
+          pdf: ["pdfjs-dist"],
+          dates: ["moment", "dayjs", "date-fns"],
+          msal: ["@azure/msal-browser", "@azure/msal-react"],
+          bootstrap: ["bootstrap", "react-bootstrap"],
+        },
+      },
+    },
   },
 });
