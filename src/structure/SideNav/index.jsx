2973276e78d4028b13edc2cb267e65b89cf3a3bd
Dany Thomas <prog3@infobahnworld.com>
2026-07-22 14:37:32 +0400

    updates

--- src/structure/SideNav/index.jsx
+++ src/structure/SideNav/index.jsx
@@ -234,7 +234,7 @@ function SideNav({ isMobileMenuOpen, onCloseMobileMenu, activePortal = null }) {
       .filter((workflow) => workflow?.role_id === null)
       .filter((workflow) => {
         const name = workflow?.workflow_name ?? workflow?.name ?? workflow?.title ?? '';
-        return name !== 'Task Workflow';
+        return name !== 'Task Workflow' && name !== 'DA Workflow';
       })
       .map((workflow) => ({
         id: workflow?.workflow_id ?? workflow?.id,
